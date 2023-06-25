3c9228bdc334435d37df03425708d4bf30efc003
yangjiajia33 <yangjiajia.33@bytedance.com>
2023-06-25 21:10:25 +0800

    feat: 注释添加

--- buildSrc/src/main/java/com/xiaojian/yous/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/xiaojian/yous/buildsrc/Dependencies.kt
@@ -2,6 +2,8 @@ package com.xiaojian.yous.buildsrc
 
 /**
  * 项目全局的依赖管理
+ *
+ * 参考自 [使用buildSrc管理项目三方依赖](https://zhuanlan.zhihu.com/p/630336016)
  */
 object Dependencies {
     const val core_ktx = "androidx.core:core-ktx:1.7.0"
