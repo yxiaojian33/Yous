055e8cf908cb52aa500aecb3942bc5906d9733df
yangjiajia33 <yangjiajia.33@bytedance.com>
2023-06-25 20:47:03 +0800

    feat: gradle版本依赖

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+}
+repositories{
+    mavenCentral()
+}
---
+++ buildSrc/src/main/java/com/xiaojian/yous/buildsrc/Dependencies.kt
@@ -0,0 +1,8 @@
+package com.xiaojian.yous.buildsrc
+
+/**
+ * 项目全局的依赖管理
+ */
+object Dependencies {
+    const val core_ktx = "androidx.core:core-ktx:1.7.0"
+}
